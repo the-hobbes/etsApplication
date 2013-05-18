24f9f050ed0fcf36c9242567306e54aafec3afb2
the-hobbes <phelan.vendeville@gmail.com>
2013-05-18 08:56:47 -0400

    initial commit before working on database hook in

--- thanks.php
+++ thanks.php
@@ -8,5 +8,15 @@
 			<h1><span style="color:green;">Thank you</span> for filling out the ETS Student Technician Application</h1>
 			<p style="text-align:center;"> You will be contacted by ETS once your application has been reviewed. </p>
 		</div> <!-- end main -->
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-40647787-1', 'uvm.edu');
+		  ga('send', 'pageview');
+
+		</script>
 	</body>
 </html>
